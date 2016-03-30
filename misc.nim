5f60793ac230eb3f9a6a60e337ee715a498c7274
Peter Cai <peter@typeblog.net>
2016-03-30 12:10:42 +0000

    initial commit

---
+++ misc.nim
@@ -0,0 +1,28 @@
+import jester, asyncdispatch, asyncfile, os, strutils, math, json
+
+var splashes: seq[string] = @[]
+
+# Load the splashes
+proc loadSplashes() {.async} =
+    let file = openAsync(CurDir & "/splash.txt", fmRead)
+    let data = await file.readAll()
+    for line in splitLines(data):
+        splashes.add(line)
+    file.close()
+
+routes:
+    get "/splash":
+        let j = %*
+            {
+                "status": 200,
+                "content": random(splashes)
+            }
+        
+        if @"callback" == nil or @"callback" == "":
+            resp $j
+        else:
+            # For jsonp requests
+            resp "/**/ typeof " & @"callback" & " === 'function' && " & @"callback" & "(" & $j & ");"
+        
+waitFor loadSplashes()
+runForever()
---
+++ typeblog_misc.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Peter Cai"
+description   = "misc.typeblog.net"
+license       = "WTFPL"
+
+bin = @["misc"]
+
+# Dependencies
+
+requires "nim >= 0.13.0"
+requires "jester#head"
